6242352f5c53732a1a808b0176a39dc4f13d1e81
savchenko.yurii <savchenko.yurii@globallogic.com>
2019-09-18 14:28:40 +0300

    fixed problem for name with spaces inside

--- RecursiveParsingFolders/archivation_of_tree_folders.sh
+++ RecursiveParsingFolders/archivation_of_tree_folders.sh
@@ -24,11 +24,17 @@ level=0
 # function for traverse
 function deep_descent {
     level=$(expr $level + 4)
-    local step="--"
+
     local dir=$1
+    local items=$(ls -1 $dir)
+
+    # save variable { Internal Field Separator } to another variable
+    oldIFS="$IFS"
+    # setup new separator
+    IFS=$'\n'
 
     # main loop
-    for item in $(ls $dir)
+    for item in $items
     do
         # printing of pointer to item
         printf %"$level"s | tr " " "-" >> tree_file_system.txt
@@ -43,13 +49,16 @@ function deep_descent {
             level=$(expr $level - 4)
         fi
     done
+
+    # restore old separator
+    IFS="$oldIFS"
 }
 
 # invoke of function
 deep_descent $root
 
-# archiving of the file
+# archiving file
 zip "archive-"$(echo $(LANG=en_us_88591; date) | tr -s [:space:] '.')'zip' tree_file_system.txt
 
-# removing of file with info
+# removing file with info
 rm tree_file_system.txt
