cb930143b8dd06854d1273819cea5b794958baea
savchenko.yurii <savchenko.yurii@globallogic.com>
2019-09-20 19:44:59 +0300

    fixing bug of writing into the file since second string

--- MorzeCipher/encrypt.sh
+++ MorzeCipher/encrypt.sh
@@ -14,7 +14,7 @@ then
     echo -e '\e[31m------------------------------------------------------------'
 else
     # clearing of the file
-    echo > $2
+    echo -n > $2
 
     # an array with a latina symbols
     declare -a latina=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z")
